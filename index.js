56b0399dd263e6f35ca8f1b44956ad55fbc341f4
AlexDyatlov <aleks78.gooogle@gmail.com>
2020-10-09 15:35:02 +0300

    added toggle

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 import { Select } from './select/select';
 import './select/styles.scss'
+
+const select = new Select('#select', {
+
+})
+
+
+
+window.s = select
--- select/select.js
+++ select/select.js
@@ -1,3 +1,71 @@
+const getTemplate = () => {
+  return `
+    <div class="select__input" data-type="input">
+      <span>Text</span>
+      <i class="fa fa-chevron-down" data-type="arrow"></i>
+    </div>
+    <div class="select__dropdown">
+      <ul class="select__list">
+        <li class="select__list-item">4</li>
+        <li class="select__list-item">3</li>
+        <li class="select__list-item">2</li>
+        <li class="select__list-item">2</li>
+        <li class="select__list-item">1</li>
+      </ul>
+    </div>
+  `
+}
+
 export class Select {
-  
+  constructor(selector, options) {
+    // $el - dom element
+    this.$el = document.querySelector(selector)
+
+    this.#render()
+    this.#setup()
+  }
+
+  #render() {
+    this.$el.classList.add('select')
+    this.$el.innerHTML = getTemplate()
+  }
+
+  #setup() {
+    this.clickHandler = this.clickHandler.bind(this)
+    this.$el.addEventListener('click', this.clickHandler)
+    this.$arrow = this.$el.querySelector('[data-type="arrow"]')
+  }
+
+  clickHandler(e) {
+    const {type} = e.target.dataset
+    
+    if (type === 'input') {
+      this.toggle()
+    }
+  }
+
+  get isOpen() {
+    return this.$el.classList.contains('open')
+  }
+
+  toggle() {
+    this.isOpen ? this.close() : this.open()
+  }
+
+  open() {
+    this.$el.classList.add('open')
+    this.$arrow.classList.remove('fa-chevron-down')
+    this.$arrow.classList.add('fa-chevron-up')
+  }
+
+  close() {
+    this.$el.classList.remove('open')
+    this.$arrow.classList.add('fa-chevron-down')
+    this.$arrow.classList.remove('fa-chevron-up')
+  }
+
+   //удаляем слушатель
+  destroy() {
+    this.$el.removeEventListener('click', this.clickHandler)
+  }
 }
