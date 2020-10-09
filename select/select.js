68a50a3792f4a596b824efddc16dda7c7f8ed5a9
AlexDyatlov <aleks78.gooogle@gmail.com>
2020-10-09 16:34:40 +0300

    final commit, added options

--- select/select.js
+++ select/select.js
@@ -1,16 +1,26 @@
-const getTemplate = () => {
+const getTemplate = (data = [], placeholder, selectedId) => {
+  let text = placeholder ?? 'Placeholder по умолчанию'
+
+  const items = data.map(item => {
+    let cls = ''
+    if (item.id === selectedId) {
+      text = item.value
+      cls = 'selected'
+    }
+    return `
+      <li class="select__list-item ${cls}" data-type="item" data-id="${item.id}">${item.value}</li>
+    `
+  })
+
   return `
+    <div class="select__backdrop" data-type="backdrop"></div>
     <div class="select__input" data-type="input">
-      <span>Text</span>
+      <span data-type="value">${text}</span>
       <i class="fa fa-chevron-down" data-type="arrow"></i>
     </div>
     <div class="select__dropdown">
       <ul class="select__list">
-        <li class="select__list-item">4</li>
-        <li class="select__list-item">3</li>
-        <li class="select__list-item">2</li>
-        <li class="select__list-item">2</li>
-        <li class="select__list-item">1</li>
+        ${items.join('')}
       </ul>
     </div>
   `
@@ -20,20 +30,24 @@ export class Select {
   constructor(selector, options) {
     // $el - dom element
     this.$el = document.querySelector(selector)
+    this.options = options
+    this.selectedId = options.selectedId
 
     this.#render()
     this.#setup()
   }
 
   #render() {
+    const {placeholder, data} = this.options
     this.$el.classList.add('select')
-    this.$el.innerHTML = getTemplate()
+    this.$el.innerHTML = getTemplate(data, placeholder, this.selectedId)
   }
 
   #setup() {
     this.clickHandler = this.clickHandler.bind(this)
     this.$el.addEventListener('click', this.clickHandler)
-    this.$arrow = this.$el.querySelector('[data-type="arrow"]')
+    this.$arrow = this.$el.querySelector('[data-type="arrow"]');
+    this.$value = this.$el.querySelector('[data-type="value"]');
   }
 
   clickHandler(e) {
@@ -41,11 +55,34 @@ export class Select {
     
     if (type === 'input') {
       this.toggle()
+    } else if (type === 'item') {
+      const id = e.target.dataset.id
+      this.select(id)
+    } else if (type === 'backdrop') {
+      this.close()
     }
   }
 
   get isOpen() {
     return this.$el.classList.contains('open')
+  }
+
+  get current() {
+    return this.options.data.find(item => item.id === this.selectedId)
+  }
+
+  select(id) {
+    this.selectedId = id
+    this.$value.textContent = this.current.value
+
+    this.$el.querySelectorAll('[data-type="item"]').forEach(el => {
+      el.classList.remove('selected')
+    })
+    this.$el.querySelector(`[data-id="${id}"]`).classList.add('selected')
+
+    this.options.onSelect ? this.options.onSelect(this.current) : null
+
+    this.close()
   }
 
   toggle() {
@@ -67,5 +104,6 @@ export class Select {
    //удаляем слушатель
   destroy() {
     this.$el.removeEventListener('click', this.clickHandler)
+    this.$el.innerHTML = ''
   }
 }
